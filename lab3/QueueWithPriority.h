79c019325173b72cc2e49777e47fcc1a8e17f980
Semyon Fomin <taxnuke@gmail.com>
2017-09-13 16:09:24 +0300

    Сливает реализации и объявления

--- lab3/QueueWithPriority.h
+++ lab3/QueueWithPriority.h
@@ -32,67 +32,62 @@ class QueueWithPriority
     private:
         elementList _hPriority, _nPriority, _lPriority;
     public:
-        void putElementToQueue(const QueueElement &, ElementPriority);
-        QueueElement getElementFromQueue();
-        QueueWithPriority accelerate();
-        QueueWithPriority render();
+        void putElementToQueue(
+            const QueueElement &element, const ElementPriority priority)
+        {
+            elementList *targetList = nullptr;
+
+            switch (priority) {
+                case HIGH: targetList = &_hPriority; break;
+                case NORMAL: targetList = &_nPriority; break;
+                case LOW: targetList = &_lPriority; break;
+            }
+
+            targetList->push_back(element);
+        }
+
+        QueueElement getElementFromQueue()
+        {
+            QueueElement element = QueueElement();
+            vector<elementList*> priorityLists = {
+                &_hPriority, &_nPriority, &_lPriority
+            };
+
+            for (auto list : priorityLists) {
+                if (!list->empty()) {
+                    element = list->front();
+                    list->pop_front();
+
+                    break;
+                }
+            }
+
+            return element;
+        };
+
+        QueueWithPriority& accelerate()
+        {
+            _hPriority.splice(_hPriority.end(), _lPriority);
+
+            return *this;
+        };
+
+        QueueWithPriority& render()
+        {
+            vector<elementList*> priorityLists = {
+                &_hPriority, &_nPriority, &_lPriority
+            };
+
+            for (auto list : priorityLists) {
+                for (const auto &element : *list) {
+                    cout << element.name << SPACE;
+                }
+            }
+
+            cout << endl;
+
+            return *this;
+        };
 };
-
-QueueWithPriority QueueWithPriority::render()
-{
-    vector<elementList*> priorityLists = {
-        &_hPriority, &_nPriority, &_lPriority
-    };
-    
-    for (auto list : priorityLists) {
-        for (const auto &element : *list) {
-            cout << element.name << SPACE;
-        }
-    }
-    
-    cout << endl;
-    
-    return *this;
-}
-
-void QueueWithPriority::putElementToQueue(
-    const QueueElement &element, const ElementPriority priority)
-{
-    elementList *targetList = nullptr;
-    
-    switch (priority) {
-        case HIGH: targetList = &_hPriority; break;
-        case NORMAL: targetList = &_nPriority; break;
-        case LOW: targetList = &_lPriority; break;
-    }
-    
-    targetList->push_back(element);
-}
-
-QueueElement QueueWithPriority::getElementFromQueue()
-{
-    QueueElement element = QueueElement();
-    vector<elementList*> priorityLists = {
-        &_hPriority, &_nPriority, &_lPriority
-    };
-    
-    for (auto list : priorityLists) {
-        if (!list->empty()) {
-            element = list->front();
-            list->pop_front();
-            
-            break;
-        }
-    }
-    
-    return element;
-}
-
-QueueWithPriority QueueWithPriority::accelerate()
-{
-    _hPriority.splice(_hPriority.end(), _lPriority);
-    
-    return *this;
-}
 
 #endif /* QueueWithPriority_h */
